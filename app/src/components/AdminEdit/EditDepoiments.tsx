434c6822215b34c302700a32f1f4a8f0809a201c
Jaqueline Silva <jscnewlf@gmail.com>
2023-08-27 21:10:35 -0300

    Subindo put EditDepoiments

--- app/src/components/AdminEdit/EditDepoiments.tsx
+++ app/src/components/AdminEdit/EditDepoiments.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 interface DepoimentItem {
     id: number;
@@ -13,48 +14,102 @@ interface EditDepoimentItemsProps {
 }
 
 export default function EditDepoimentItems({ depoimentItems, onUpdate }: EditDepoimentItemsProps) {
-  const handleInputChange = (index: number, field: keyof DepoimentItem, value: string) => {
-    const updatedDepoimentItems = [...depoimentItems];
+
+  const [uploadingImages, setUploadingImages] = useState<boolean[]>(new Array(depoimentItems.length).fill(false));
+  const [updatedItems, setUpdatedItems] = useState<DepoimentItem[]>(depoimentItems);
+
+  const handleInputChange = (index: number, field: keyof DepoimentItem, value: string | boolean) => {
+    const updatedDepoimentItems = [...updatedItems];
     updatedDepoimentItems[index] = {
       ...updatedDepoimentItems[index],
       [field]: value,
     };
-    onUpdate(updatedDepoimentItems[index], updatedDepoimentItems[index].id);
+
+    setUpdatedItems(updatedDepoimentItems);
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, index: number) => {
     event.preventDefault();
+
+    const selectedFileInput = event.currentTarget.querySelector<HTMLInputElement>(`input[type="file"][name="image-${index}"]`);
+    
+    if (selectedFileInput && selectedFileInput.files && selectedFileInput.files.length > 0) {
+      const selectedFile = selectedFileInput.files[0];
+      await handleImageChange(index, selectedFile);
+
+      return;
+    }
+
+    onUpdate(updatedItems[index], updatedItems[index].id);
+    alert('Updated successfully');
+  };
+
+  const handleImageChange = async (index: number, file: File) => {
+    try {
+      const formData = new FormData();
+      formData.append('image-article', file);
+
+      const response = await axios.post('http://localhost:8080/api/images/upload', formData);
+      const imageUrl = response.data.path;
+
+      const updatedDepoimentItems = [...updatedItems];
+      updatedDepoimentItems[index] = {
+        ...updatedDepoimentItems[index],
+        avatar: imageUrl,
+      };
+
+      setUpdatedItems(updatedDepoimentItems);
+      setUploadingImages((prevUploads) => {
+        const updatedUploads = [...prevUploads];
+        updatedUploads[index] = false;
+        return updatedUploads;
+      });
+
+      onUpdate(updatedDepoimentItems[index], updatedDepoimentItems[index].id);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
   };
 
   return (
     <div>
-      <div className='flex'>
-        <p>Texto</p>
-        <p>Link</p>
+      <div className='flex border-b-2 border-dark pb-2 mb-2'>
+        <p className='w-10 pl-1 mr-5'>Item</p>
+        <p className='w-[205px]'>Name</p>
+        <p className='w-[205px]'>Comment</p>
+        <p>Avatar</p>
       </div>
       <div>
         {depoimentItems.map((depoimentItem, index) => (
-          <form key={index} onSubmit={(e) => handleSubmit(e, index)}>
-            <div className='flex'>
-              <div>
-                <h3>Item {index + 1}:</h3>
+          <form className="even:bg-neutral-200" key={index} onSubmit={(e) => handleSubmit(e, index)}>
+            <div className='flex py-2 rounded my-2 flex-wrap'>
+              <div className='w-10 text-center mr-5'>
+                <p className='font-lato'>{index + 1}</p>
               </div>
-              <div>
-                <input
+              <div className='mr-5'>
+                <input className='bg-transparent border-2 border-dark rounded pl-2'
                   type='text'
-                  value={depoimentItem.name}
+                  defaultValue={depoimentItem.name}
                   onChange={(e) => handleInputChange(index, 'name', e.target.value)}
                 />
               </div>
-              <div>
-                <input
+              <div className='mr-5'>
+                <input className='bg-transparent border-2 border-dark rounded pl-2'
                   type='text'
-                  value={depoimentItem.avatar}
-                  onChange={(e) => handleInputChange(index, 'avatar', e.target.value)}
+                  defaultValue={depoimentItem.comment}
+                  onChange={(e) => handleInputChange(index, 'comment', e.target.value)}
                 />
               </div>
+              <div className='mr-3'>
+                <input id="inputFile" className='w-[122px]'
+                  type='file'
+                  name={`image-${index}`}
+                  accept='image/*'
+                />
+              </div>
+              
               <div>
-                <button type='submit'>Update</button>
+                <button className="bg-dark text-white border-dark py-1 w-[200px] block text-center rounded-md hover:opacity-90 " type='submit'>Update</button>
               </div>
             </div>
           </form>
